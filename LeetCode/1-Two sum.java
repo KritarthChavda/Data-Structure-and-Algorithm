2dd801a672d751899d7e33f80d8e5c30fd016de6
KritarthChavda <113841682+KritarthChavda@users.noreply.github.com>
2023-02-25 20:05:50 +0530

    Update 1-Two sum.java

--- LeetCode/1-Two sum.java
+++ LeetCode/1-Two sum.java
@@ -1,7 +1,10 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         //now instead of finding two numbers that add up to the target 
-        //what we do is , we iterate once in the array and calculate the   target - the integer and put the integer and its index in the hash map , now if the complement is in the hash map , we found our answer ( the element we are on and the index from the hash map )
+        //what we do is , we iterate once in the array and calculate the
+        //target - the integer and put the integer and its index in the hash map
+        //now if the complement is in the hash map
+        //we found our answer ( the element we are on and the index from the hash map )
         Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
             int complement = target - nums[i];
